7a62c958f430b8665af8a84af70db7f87bd61b07
reyn-nova <rynvva@gmail.com>
2023-09-18 12:59:14 +0700

    add login page UI

--- screens/Login.js
+++ screens/Login.js
@@ -1,42 +1,110 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {useForm} from 'react-hook-form';
+
+import Input from '../components/Input';
 
 const Login = props => {
-  return (
-    <View
-      style={{
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          fontSize: 28,
-          fontWeight: 'bold',
-        }}>
-        Login
-      </Text>
+  const submitLogin = () => {
+    props.navigation.replace('BottomTabs');
+  };
 
-      <TouchableOpacity
-        onPress={() => props.navigation.replace('BottomTabs')}
-        style={{
-          backgroundColor: 'teal',
-          borderRadius: 10,
-          marginTop: 20,
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-        }}>
-        <Text
-          style={{
-            color: 'white',
-            fontWeight: 'bold',
-            fontSize: 20,
-          }}>
-          Go To Bottom Tabs
-        </Text>
-      </TouchableOpacity>
-    </View>
+  const {
+    control,
+    formState: {isValid},
+  } = useForm();
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tour App</Text>
+
+        <View style={styles.textInputsContainer}>
+          <Input
+            control={control}
+            name="username"
+            autoComplete="username"
+            autoCorrect={false}
+            placeholder="Username"
+            style={styles.textInput}
+            placeholderTextColor="darkgray"
+            required={true}
+          />
+
+          <Input
+            control={control}
+            name="password"
+            autoComplete="password"
+            autoCorrect={false}
+            placeholder="Password"
+            style={styles.textInput}
+            placeholderTextColor="darkgray"
+            secureTextEntry
+            required={true}
+          />
+        </View>
+
+        <TouchableOpacity
+          onPress={submitLogin}
+          disabled={!isValid}
+          style={[
+            styles.button,
+            {
+              backgroundColor: isValid ? 'mediumseagreen' : 'dimgray',
+            },
+          ]}>
+          <Text style={styles.buttonLabel}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  textInputsContainer: {
+    width: '100%',
+  },
+  textInput: {
+    backgroundColor: 'white',
+    color: 'black',
+    width: '100%',
+    padding: 10,
+    height: 40,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  button: {
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonLabel: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default Login;
